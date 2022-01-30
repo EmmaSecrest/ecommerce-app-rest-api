2012202238782f767e9b6b45e6796487f8d601ee
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2022-01-30 18:32:40 -0500

    order route completed

--- createCartDatabase.sql
+++ createCartDatabase.sql
@@ -1,61 +1,9 @@
-CREATE TABLE users (
-  id int,
-  password char(50),
-  email varchar(50),
-  first_name varchar(50),
-  last_name varchar(50),
-  created TIMESTAMP,
-  modified timestamp,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE orders (
-  id int,
-  total money,
-  status varchar(30), -- this could be boolean as well
-  user_id int,
-  created TIMESTAMP,
-  modified timestamp,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-CREATE TABLE product (
-  id int,
-  name varchar(100),
-  description varchar(250),
-  created TIMESTAMP,
-  modified timestamp,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE order_item (
-  id int,
-  quantity int,
-  price money,
-  order_id int,
-  product_id int,
-  created TIMESTAMP,
-  modified timestamp,
-  PRIMARY KEY (id),
-  FOREIGN KEY (order_id) REFERENCES orders(id),
-  FOREIGN KEY (product_id) REFERENCES product(id)
-);
-
-CREATE TABLE cart (
-  id int,
-  created TIMESTAMP,
-  modified timestamp,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE cart_item (
-  product_id int,
-  cart_id int,
-  created  TIMESTAMP,
-  modified timestamp,
-  FOREIGN KEY (product_id) REFERENCES product(id),
-  FOREIGN KEY (cart_id) REFERENCES cart(id)
-);
-
--- insert test
+INSERT INTO product (name,price,id)
+VALUES('BTS BT21 Headband (7 Types) RJ' ,14.95 ,9),
+  ('BTS BT21 Headband (7 Types) Shooky' ,14.95 ,10),
+  ('BTS BT21 Headband (7 Types) Mang' ,14.95 ,11),
+  ('BTS BT21 Headband (7 Types) Koya' ,14.95 ,12),
+  ('BTS BT21 Headband (7 Types) Chimmy' ,14.95 ,13),
+  ('BTS BT21 Headband (7 Types) Tata' ,14.95 ,14),
+  ('BTS BT21 Headband (7 Types) Cooky' ,14.95 ,15),
+  ('Cartoon Projector Pendant Necklace - BTS' , 21.90, 16);
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,102 @@
 const express = require("express");
+const { pool } = require("../config");
 const orderRouter = express.Router();
+const {ensureAuthentication} = require('./login')
 
-orderRouter.get('/', (req,res) => {
-    res.send('orders')
+//needed functions
+const orderExists = (orderId ,done) =>{
+    pool.query('SELECT * FROM orders WHERE id = $1' ,[orderId],
+    function(err,data){
+        if(err){
+            console.log(err)
+            done(err,null)
+        } else{
+            done(null,data)
+        }
+    })
+}
+
+const checkOrder = (orderId , done) =>{
+    pool.query('SELECT * FROM orders WHERE id = $1 and status = $2' ,[orderId ,'received'],
+    function(err, data){
+        if (err){
+            console.log(err)
+            done(err,null)
+        } else {
+            done(null,data)
+        }
+    })
+}
+
+//get all orders
+orderRouter.get('/',ensureAuthentication ,(req,res) => {
+    const customersOrders = Number(req.user.id)
+    pool.query('SELECT total, status  FROM orders WHERE user_id = $1',[customersOrders] ,function(err, data){
+        if(err){
+            console.log(err)
+            res.send("There was an issue getting that order")
+        } else{
+            if(data.rowCount === 0){
+                res.send("That order does not exist")
+            } else{
+                res.send(data.rows)
+            }
+        }
+    })
 });
+
+// view a specific order
+orderRouter.get('/:orderId',ensureAuthentication,(req,res)=>{
+    const requestedOrder = Number(req.params.orderId)
+    pool.query('select orders.id as order_id, order_item.product_id as product_id, product.name as name ,product.price as price FROM orders join order_item on orders.id = order_item.order_id join product on order_item.product_id = product.id WHERE orders .id =$1',[requestedOrder], 
+    function(err,received){
+        if(err){
+            console.log(err)
+            res.send("Can't grab this order")
+        } else{
+            if(received.rowCount === 0){
+                res.send('This order is empty')
+            } else {
+                res.send(received.rows)
+            }
+        }
+    })
+})
+
+// delete an order if not shipped
+orderRouter.delete('/:cartId',(req,res)=>{
+    const orderId = req.params.cartId
+    orderExists(orderId ,function(err,result){
+        if (err){
+            throw err;
+        } else{
+            if(result.rowCount === 0){
+                res.send("Order does not exist")
+            } else {
+                checkOrder(orderId, function(err, data){
+                    if(data.rowCount === 0){
+                        res.send("Order Has shipped cannot delete")
+                    } else{
+                        pool.query('DELETE FROM order_item WHERE order_id =$1', [orderId], function(err,d){
+                            if(err){
+                                console.log(err)
+                            } else {
+                                pool.query('DELETE FROM orders where id = $1',[orderId],
+                                function(err,dat){
+                                    if(err){
+                                        console.log(err)
+                                    } else{
+                                        res.send("order successfully deleted")
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }  
+        }
+    })
+} )
 
 module.exports = orderRouter
 
